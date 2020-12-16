5f0e5157204202ec096e7153cc59b84e633d0838
Sabrina Kuah <sabrina@Sabrinas-MacBook-Pro.local>
2020-12-15 16:29:17 -0800

    contact form added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { pageTransitionSlow, pageWipe } from "./common/Animation";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion";
+import { FiGithub, FiLinkedin } from "react-icons/fi";
 
 export default class Contact extends Component {
   render() {
@@ -14,69 +15,6 @@ export default class Contact extends Component {
         transition={pageTransitionSlow}
         className="landing"
       >
-        <div id="landing-board">
-          <div className="board-wrapper">
-            <div className="frame-outer">
-              <div className="frame-inner">
-                {/* Contact Form */}
-
-                <div class="container">
-                  <div class="poster">
-                    <div className="contact-form">
-                      <form>
-                        <div class="form-group">
-                          <label for="exampleInputEmail1">Email address</label>
-                          <input
-                            type="email"
-                            class="form-control"
-                            id="exampleInputEmail1"
-                            aria-describedby="emailHelp"
-                            placeholder="Enter email"
-                          />
-                          <small id="emailHelp" class="form-text text-muted">
-                            We'll never share your email with anyone else.
-                          </small>
-                        </div>
-                        <div class="form-group">
-                          <label for="exampleInputPassword1">Password</label>
-                          <input
-                            type="password"
-                            class="form-control"
-                            id="exampleInputPassword1"
-                            placeholder="Password"
-                          />
-                        </div>
-                        <div class="form-group">
-                          <label for="exampleFormControlTextarea1">
-                            Example textarea
-                          </label>
-                          <textarea
-                            class="form-control"
-                            id="exampleFormControlTextarea1"
-                            rows="3"
-                          ></textarea>
-                        </div>
-                        <div class="form-check">
-                          <input
-                            type="checkbox"
-                            class="form-check-input"
-                            id="exampleCheck1"
-                          />
-                          <label class="form-check-label" for="exampleCheck1">
-                            Check me out
-                          </label>
-                        </div>
-                        <button type="submit" class="btn btn-primary">
-                          Submit
-                        </button>
-                      </form>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
         <div id="landing-message">
           <div id="welcome">
             <img
@@ -84,16 +22,65 @@ export default class Contact extends Component {
               src="profile_cartoon.svg"
               alt="cartoon of me"
             />
-            <h1>Let's chat!</h1>
-            <p>perhaps I can be of service</p>
-            {/* <p>Full-Stack Web Developer, Educator</p> */}
+            <h1>Send me a message</h1>
             <div className="row" id="landing-buttons">
-              <Link to="/portfolio">
-                <li className="l-btn">See Portfolio</li>
+              <Link to="/">
+                <li className="l-btn">
+                  GitHub
+                  <FiGithub className="icon mx-2" />
+                </li>
               </Link>
-              <Link to="/about">
-                <button className="l-btn">About Me</button>
+              <Link to="/">
+                <button className="l-btn">
+                  LinkedIn
+                  <FiLinkedin className="icon mx-2" />
+                </button>
               </Link>
+            </div>
+          </div>
+        </div>
+        <div id="landing-board">
+          <div className="board-wrapper">
+            <div className="frame-outer">
+              <div className="frame-inner">
+                {/* Contact Form */}
+                <div class="container contact-container">
+                  <div className="note" id="contact-form">
+                    <div className="tape" />
+                    <div className="note-content px-3">
+                      <h2>Contact Me</h2>
+                      <form>
+                        <div class="form-group py-3">
+                          <label for="inputEmail4">Your Email</label>
+                          <input
+                            type="email"
+                            class="form-control"
+                            id="inputEmail4"
+                            placeholder="Email"
+                          />
+                        </div>
+                        <div class="form-group py-3">
+                          <label for="exampleFormControlTextarea1">
+                            Message
+                          </label>
+                          <textarea
+                            class="form-control"
+                            id="exampleFormControlTextarea1"
+                            rows="8"
+                            placeholder="Your message"
+                          ></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-success">
+                          Submit
+                        </button>
+                      </form>
+                    </div>
+
+                    <div className="left-shadow"></div>
+                    <div className="right-shadow"></div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
